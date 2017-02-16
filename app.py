b991b6de43cb7bc627e88dbe6da39fa577dd417f
Michel Ribeiro <MichelRibeiro1@users.noreply.github.com>
2017-02-16 00:08:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import csv, json
 
 salas, sala_aux, sala_aux2 = [], '', []
 
-#Adicionar baixo o nome do arquivo a ser convertido, considerando que esteja no mesmo diretório
-
-with open('salas-sinternet.csv', 'r') as csvfile: 
-	salas_lista = csv.reader(csvfile)
-	for sala in salas_lista:
-		salas.append(sala)
-		if sala[0] == 'Sem':
-			salas.remove(salas[salas.index(sala)])
-		elif sala[10] != '':
-			sala_aux = sala[10]
-			sala_aux2.append({'Semestre': sala[0], 'Disciplina': sala[1], 'Professor': sala[2], 'Curso': sala[3], 'Código': sala[4], 'Dia': sala[5], 'Horário Inicial': sala[6], 'Horário Final': sala[7], 'Lab': sala[8], 'Compartilhada': sala[9], 'Sala': sala[10]})
-		else:
-			sala[10] = sala_aux
-			sala_aux2.append({'Semestre': sala[0], 'Disciplina': sala[1], 'Professor': sala[2], 'Curso': sala[3], 'Código': sala[4], 'Dia': sala[5], 'Horário Inicial': sala[6], 'Horário Final': sala[7], 'Lab': sala[8], 'Compartilhada': sala[9], 'Sala': sala[10]})
-		
+for arquivo in range(1,11):
+	arq = str(arquivo) + '.csv'
+	with open(arq, 'r') as csvfile: 
+		salas_lista = csv.reader(csvfile)
+		for sala in salas_lista:
+			salas.append(sala)
+			if sala[0] == 'Sem':
+				salas.remove(salas[salas.index(sala)])
+			elif sala[10] != '':
+				sala_aux = sala[10]
+				sala_aux2.append({'sem': sala[0], 'dis': sala[1], 'prof': sala[2], 'curso': sala[3], 'codTurma': sala[4], 'dia': sala[5], 'hInicio': sala[6], 'hFim': sala[7], 'lab': sala[8], 'comp': sala[9], 'sala': sala[10]})
+			else:
+				sala[10] = sala_aux
+				sala_aux2.append({'sem': sala[0], 'dis': sala[1], 'prof': sala[2], 'curso': sala[3], 'codTurma': sala[4], 'dia': sala[5], 'hInicio': sala[6], 'hFim': sala[7], 'lab': sala[8], 'comp': sala[9], 'sala': sala[10]})
+			
 	print(json.dumps(sala_aux2))
