fbdea60a5ff258759f019e4345571c1e641b6a48
Michel Ribeiro <MichelRibeiro1@users.noreply.github.com>
2017-02-20 11:01:21 -0300

    Update app.py
    Added ".strip()"

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import csv, json
 
 salas, sala_aux, sala_aux2= [], '', [] 
 
-arq2 = open('classes2.txt', 'w')
+arq2 = open('classes2-ts.txt', 'w')
 
 #Adicionar baixo o nome do arquivo a ser convertido, considerando que esteja no mesmo diretório
 
-with open('9.csv', 'r') as csvfile: 
+with open('12.csv', 'r') as csvfile: 
 	salas_lista = csv.reader(csvfile)
 	for sala in salas_lista:
 		salas.append(sala)
@@ -15,10 +15,10 @@ with open('9.csv', 'r') as csvfile:
 		else: 
 			if sala[10] != '':
 				sala_aux = sala[10]
-				sala_aux2.append({'sem': sala[0], 'dis': sala[1], 'prof': sala[2], 'curso': sala[3], 'codTurma': sala[4], 'dia': sala[5], 'hInicio': sala[6], 'hFim': sala[7], 'lab': sala[8], 'comp': sala[9], 'sala': sala[10]})
+				sala_aux2.append({'sem': sala[0].strip(), 'dis': sala[1].strip(), 'prof': sala[2].strip(), 'curso': sala[3].strip(), 'codTurma': sala[4].strip(), 'dia': sala[5].strip(), 'hInicio': sala[6].strip(), 'hFim': sala[7].strip(), 'lab': sala[8].strip(), 'comp': sala[9].strip(), 'sala': sala[10].strip()})
 			else:
 				sala[10] = sala_aux
-				sala_aux2.append({'sem': sala[0], 'dis': sala[1], 'prof': sala[2], 'curso': sala[3], 'codTurma': sala[4], 'dia': sala[5], 'hInicio': sala[6], 'hFim': sala[7], 'lab': sala[8], 'comp': sala[9], 'sala': sala[10]})
+				sala_aux2.append({'sem': sala[0].strip(), 'dis': sala[1].strip(), 'prof': sala[2].strip(), 'curso': sala[3].strip(), 'codTurma': sala[4].strip(), 'dia': sala[5].strip(), 'hInicio': sala[6].strip(), 'hFim': sala[7].strip(), 'lab': sala[8].strip(), 'comp': sala[9].strip(), 'sala': sala[10].strip()})
 			
 arq2.write(json.dumps(sala_aux2))
 arq2.close()
